b2bc32e0d4c0b1bc05fb299ebd4e5a8a07fcc2eb
Matthew Martin <s1675498@allendale.inf.ed.ac.uk>
2016-11-18 15:10:21 +0000

    task files

---
+++ src/taskA.py
@@ -0,0 +1,5 @@
+# TASK A
+import robot
+if __name__ == '__main__':
+    r = robot.Robot()
+    r.go()
---
+++ src/taskC.py
@@ -0,0 +1,8 @@
+# TASK C
+import robot
+import linefollower as lfw
+
+if __name__ == '__main__':
+    r = robot.Robot()
+    r.changeFollowerType(lfw.BrokenLineFollower())
+    r.go()
